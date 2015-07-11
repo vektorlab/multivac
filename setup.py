3da2dedde55530ed08fbc7e89f12198200053682
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-07-11 13:19:33 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from slackbot import __version__
+
+setup(name='slackbot',
+      version=__version__,
+      packages=['slackbot'],
+      description='Slack RTM Chatops Bot',
+      author='Bradley Cicenas',
+      author_email='bradley.cicenas@gmail.com',
+      url='https://github.com/bcicen/slackbot',
+      install_requires=['slacksocket>=0.4.4'],
+      license='http://opensource.org/licenses/MIT',
+      classifiers=(
+          'License :: OSI Approved :: MIT License ',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ),
+      keywords='slack rtm websocket api chatops bot')
