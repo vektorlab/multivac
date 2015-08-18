7cf7abf434f9d104dadd1f692ecd741132164d73
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-08-18 14:52:46 -0400

    add job_worker to spawn subprocess and logger thread

--- slackbot/manager.py
+++ slackbot/manager.py
@@ -8,6 +8,7 @@ import os
 from time import sleep
 from redis import StrictRedis
 from threading import Thread
+from copy import deepcopy
 
 from util import unix_time
 from models import JobsDB
@@ -18,7 +19,7 @@ class Manager(object):
     def __init__(self, redis_host, redis_port, config_path='config.yml'):
         self.config_path = config_path
         self.db = JobsDB(redis_host, redis_port)
-        self.procs = {} #dict of job_id:(process obj,process pipe object)
+        self.pids = {} #dict of job_id:subprocess object
 
         self._load_actions()
         self.run()
@@ -32,19 +33,12 @@ class Manager(object):
                 self._start_job(job)
 
             #collect ended processes
-            collected = []
-            for job_id in self.procs:
-                exitcode = self.procs[job_id].returncode
-                if exitcode and exitcode == 0:
+            pids = deepcopy(self.pids)
+            for job_id,pid in pids.iteritems():
+                if not self._is_running(pid):
                     self.db.update_job(job_id, 'status', 'completed')
-                    collected.append(job_id)
-                if exitcode and exitcode != 0:
-                    self.db.update_job(job_id, 'status', 'failed')
-                    collected.append(job_id)
-
-            for j in collected:
-                log.info('Collected ended job %s' % j)
-                del self.procs[j]
+                    log.info('Collected ended job %s' % job_id)
+                    del self.pids[job_id]
 
             sleep(1)
 
@@ -64,39 +58,45 @@ class Manager(object):
             log.info('loaded action %s' % (action['name']))
 
     def _start_job(self, job):
-        job_id = job['id']
-        self.db.update_job(job_id, 'status', 'running')
+        worker = Thread(target=self._job_worker,args=[job])
+        worker.daemon = True
+        worker.start()
+
+        #thread.join(timeout=1)
+
+    def _is_running(self, pid):
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            return False
+        return True
+
+    def _job_worker(self, job):
+        log.debug('Worker spawned for job %s' % job['id'])
+        self.db.update_job(job['id'], 'status', 'running')
 
         if job['args']:
-            cmd = shlex.split(job['cmd'] + ' ' + job['args'])
+            cmdline = shlex.split(job['cmd'] + ' ' + job['args'])
         else:
-            cmd = job['cmd']
+            cmdline = job['cmd']
 
         proc = subprocess.Popen(
-                cmd,
+                cmdline,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
 
-        self.procs[job_id] = proc
+        self.pids[job['id']] = proc.pid
 
-        thread = Thread(
+        logger = Thread(
                 target=self._log_worker,
-                args=[job_id, proc.stdout, proc.stderr])
-        thread.daemon = True
-        thread.start()
-        thread.join(timeout=1)
+                args=[job['id'], proc.stdout, proc.stderr])
+        logger.daemon = True
+        logger.start()
 
-    def _worker(self, job, conn):
-        """ Worker to run jobs """
-        log.info('Worker spawned for job %s' % (job['id']))
-
-        #output = job.action._run()
-        conn.send('test')
-        sleep(5)
-
-        return
+        proc.wait()
 
     def _log_worker(self, job_id, stdout, stderr):
+        log.debug('Log handler started for job %s' % job_id)
         while True:
             output = self._read(stdout).strip()
             error = self._read(stderr).strip()
@@ -106,6 +106,9 @@ class Manager(object):
             if error:
                 self.db.append_log(job_id, error)
                 log.debug('%s-STDOUT: %s' % (job_id,error))
+            if job_id not in self.pids:
+                log.debug('Log handler stopped for job %s' % job_id)
+                return
 
     def _read(self, pipe):
         """
