f32f428c10ae728c1ad58a74d9618ffc8e0abd0e
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-08-19 14:21:03 -0400

    add decode methods to JobsDB for python3 byte > unicode conversion

--- slackbot/models.py
+++ slackbot/models.py
@@ -1,6 +1,7 @@
 from redis import StrictRedis
 from datetime import datetime
 from uuid import uuid4
+from time import sleep
 
 class JobsDB(object):
     job_prefix = 'multivac_job'
@@ -45,21 +46,62 @@ class JobsDB(object):
         self.redis.hmset('multivac_job:' + job_id, job)
 
     def get_job(self, job_id):
-        return self.redis.hgetall(self._jobkey(job_id))
+        """
+        Return single job dict given a job id
+        """
+        return self._format_dict(self.redis.hgetall(self._jobkey(job_id)))
 
     def get_jobs(self, status=None):
+        """
+        Return all jobs dicts, optionally filtered by status 
+        via the 'status' param
+        """
         jobs = [ self.redis.hgetall(k) for k in \
                  self.redis.keys(pattern=self._jobkey('*')) ]
         if status:
-            return [ j for j in jobs if j['status'] == status ]
+            return [ self._format_dict(j) for j in jobs if j['status'] == status ]
         else:
-            return [ j for j in jobs ]
+            return [ self._format_dict(j) for j in jobs ]
+
+    def get_job_logstream(self, job_id):
+        """
+        Returns a generator object to stream job output
+        until the job has completed 
+        """
+        last_length = 0
+        endstream = False
+        while True:
+            cur_length = self.redis.llen(self._logkey(job_id))
+
+            print(cur_length)
+            print(last_length)
+            if cur_length > last_length:
+                count = cur_length - last_length
+                for i in reversed(range(0,count+1)):
+                    line = self.redis.lindex(self._logkey(job_id),i)
+                    if line:
+                        yield self._format_str(line)
+                last_length = cur_length
+
+            if endstream:
+                break
+
+            if self.get_job(job_id)['status'] == 'completed':
+                # wait after job is completed to ensure we collect all logs
+                sleep(1)
+                endstream = True
 
     def get_log(self, job_id):
+        """
+        Return the output log of a given job id
+        """
         logs = self.redis.lrange(self._logkey(job_id),0,-1)
         return [ l for l in reversed(logs) ]
 
     def append_log(self, job_id, line):
+        """
+        Append a line of job output to a redis list
+        """
         self.redis.lpush(self._logkey(job_id), self._append_ts(line))
 
     def _append_ts(self, msg):
@@ -71,9 +113,15 @@ class JobsDB(object):
     #######
 
     def get_action(self, action_name):
-        return self.redis.hgetall(self._actionkey(action_name))
+        """
+        Return a single action dict, given the action name
+        """
+        return self._format_dict(self.redis.hgetall(self._actionkey(action_name)))
 
     def get_actions(self):
+        """
+        Return all configured actions
+        """
         return [ self.redis.hgetall(k) for k in \
                  self.redis.keys(pattern=self._actionkey('*')) ]
 
@@ -97,3 +145,14 @@ class JobsDB(object):
     def _jobkey(self,id):
         return self.job_prefix + ':' + id
 
+    def _format_dict(self, d):
+        ret = {}
+        for k,v in d.items():
+            k = k.decode('unicode_escape')
+            v = v.decode('unicode_escape')
+            ret[k] = v 
+
+        return ret
+
+    def _format_str(self, s):
+        return s.decode('unicode_escape')
