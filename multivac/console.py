df073ce3966c3c0c9ff2fb9535322cdbdcce7fae
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-08-29 14:30:44 -0400

    add autocomplete, history to chatconsole

--- multivac/console.py
+++ multivac/console.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import readline
 from time import sleep
 from termcolor import colored
 
@@ -8,13 +10,13 @@ from multivac.util import format_time
 
 class ConsoleBot(ChatBot):
     def __init__(self, redis_host, redis_port):
-        self.prompt = colored('multivac> ','cyan',attrs=['bold']) 
-        self.resprompt = colored('> ','red',attrs=['bold'])
         self._messages =  []
         self._wait = False
 
         super().__init__(redis_host, redis_port)
-        self.run()
+
+        self.init_readline()
+        self.input_loop()
 
     @property
     def messages(self):
@@ -33,24 +35,53 @@ class ConsoleBot(ChatBot):
         else:
             self._output(msg)
 
-    def run(self):
+    def init_readline(self):
+        self.prompt = colored('multivac> ','cyan',attrs=['bold'])
+        self.resprompt = colored('> ','red',attrs=['bold'])
+
+        self._history_file = os.path.expanduser('~/.multivac_console')
+        if os.path.isfile(self._history_file):
+            readline.read_history_file(self._history_file)
+
+        readline.set_completer(self._autocomplete)
+        readline.parse_and_bind('tab: complete')
+
+    def input_loop(self):
         print('Multivac version %s' % (version))
         while True:
             try:
-                cmdline = input(self.prompt)
-            except EOFError:
-                sys.exit(0)
-            if cmdline.split(' ')[0] == 'exit':
-                sys.exit(0)
+                inputline = input(self.prompt)
+                if not inputline:
+                    continue
 
-            self._messages.append((cmdline,'console','console'))
-            self._wait = True
+                #append to messages queue for processing
+                self._messages.append((inputline,'console','console'))
 
-            while self._wait:
-                sleep(.1)
+                #wait until output of command is completed
+                self._wait = True
+                while self._wait:
+                    sleep(.1)
+
+            except (EOFError, KeyboardInterrupt):
+                break
+
+        self.exit()
+
+    def exit(self):
+        readline.write_history_file(self._history_file)
+        print('\n')
+        sys.exit(0)
 
     def _output(self, text):
         print(self.resprompt + text)
+
+    def _autocomplete(self, text, state):
+        allcmds = [ c for c in self.builtins ]
+        allcmds += [ a['name'] for a in self.db.get_actions() ]
+        match = [ c for c in allcmds if c and c.startswith(text) ]
+        match.append(None)
+
+        return match[state]
 
     @staticmethod
     def _print_column(data, has_header=True):
