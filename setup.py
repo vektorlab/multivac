c8ab262872aca620ab207572c4477c9abf315e9c
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-09-28 13:33:53 -0400

    update setup.py and package name for pypi submission

--- setup.py
+++ setup.py
@@ -2,26 +2,35 @@ from setuptools import setup
 
 exec(open('multivac/version.py').read())
 
-setup(name='multivac',
-      version=version,
-      packages=['multivac'],
-      description='Slack RTM Chatops Bot',
-      author='Bradley Cicenas',
-      author_email='bradley.cicenas@gmail.com',
-      url='https://github.com/bcicen/multivac',
-      install_requires=['slacksocket>=0.5.1'],
-      package_data={'multivac': ['templates/*', 'static/*']},
-      include_package_data=True,
-      license='http://opensource.org/licenses/MIT',
-      classifiers=(
-          'License :: OSI Approved :: MIT License ',
-          'Natural Language :: English',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-      ),
-      keywords='slack rtm websocket api chatops bot',
-      entry_points={
-          'console_scripts': ['multivac = multivac.cli:main']
-      }
-      )
+requirements = [ 'slacksocket>=0.6.0',
+                 'Flask>=0.10.1',
+                 'Flask-RESTful>=0.3.2',
+                 'gevent>=1.1b1',
+                 'redis>=2.10.3',
+                 'names>=0.3.0',
+                 'PyYAML>=3.11',
+                 'termcolor>=1.1.0' ]
+
+setup(
+    name='multivac-bot',
+    version=version,
+    packages=['multivac'],
+    description='Extensible ChatOps framework with built-in support for Slack',
+    author='Bradley Cicenas',
+    author_email='bradley@vektor.nyc',
+    url='https://github.com/vektorlab/multivac',
+    install_requires=requirements,
+    package_data={ 'multivac': ['templates/*', 'static/*'] },
+    include_package_data=True,
+    license='http://opensource.org/licenses/MIT',
+    classifiers=(
+        'License :: OSI Approved :: MIT License ',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Development Status :: 5 - Production/Stable',
+    ),
+    keywords='slack rtm websocket api chatops bot',
+    entry_points={ 'console_scripts': ['multivac = multivac.cli:main'] }
+)
